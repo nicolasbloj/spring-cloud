da216f9ac6e630193822bea3592263761554d431
nicolas <nicolasbloj@gmail.com>
2019-11-29 00:36:56 -0300

    enable circuit breaker in greeting

--- greeting/src/main/java/com/nab/greeting/GreetingApplication.java
+++ greeting/src/main/java/com/nab/greeting/GreetingApplication.java
@@ -2,8 +2,10 @@ package com.nab.greeting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 
 /**
@@ -12,6 +14,8 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 @SpringBootApplication
 @EnableDiscoveryClient
 @RefreshScope
+@EnableHystrix
+@EnableCircuitBreaker
 public class GreetingApplication {
 
 	public static void main(String[] args) {
